dd4847222f7e1d19acf4ca4e495b782d3f0e2708
jwa127084 <jwa127084@iet-gibb.ch>
2022-03-03 13:59:18 +0100

    add basic routes

--- test/controllers/subjects_controller_test.rb
+++ test/controllers/subjects_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class SubjectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get subjects_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get subjects_show_url
+    assert_response :success
+  end
 end
